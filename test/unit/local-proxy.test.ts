637ae0f252f27fd19b94d9a278f8b2ad1efa9646
wmxpy <wm@mengw.io>
2022-07-06 22:58:56 -0500

    add unit test, release

--- test/unit/local-proxy.test.ts
+++ test/unit/local-proxy.test.ts
@@ -5,15 +5,42 @@
  * @override Unit Test
  */
 
+import { SudoRPCCallManager } from "@sudorpc/core";
 import { expect } from "chai";
 import * as Chance from "chance";
+import { SudoRPCLocalProxy } from "../../src";
+import { createEchoerService } from "../mock/echoer";
 
 describe('Given {SudoRPCLocalProxy} Class', (): void => {
 
-    const chance: Chance.Chance = new Chance('placeholder');
+    const chance: Chance.Chance = new Chance('client-local-local-proxy');
 
-    it('Placeholder', (): void => {
+    let callManager: SudoRPCCallManager<any, any, any, any> = undefined as any;
 
-        expect(chance.string()).to.be.not.equal(chance.string());
+    beforeEach((): void => {
+        callManager = SudoRPCCallManager.create(
+            SudoRPCLocalProxy.create(createEchoerService()),
+        );
+        callManager.ignite();
+    });
+
+    afterEach((): void => {
+        callManager.dialDown();
+    });
+
+    it('should be able to execute with local proxy', async (): Promise<void> => {
+
+        const key: string = chance.string();
+        const value: string = chance.string();
+
+        const result: any = await callManager.makeCall("echo", {}, {
+            [key]: value,
+        });
+
+        expect(result).to.be.deep.equal({
+            echo: {
+                [key]: value,
+            },
+        });
     });
 });
